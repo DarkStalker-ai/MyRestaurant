ec45ac095c223e0fcc7023b91ab8b568f15d67c5
dark_st <tatarinov.tima6@gmail.com>
2025-06-07 16:35:18 +0300

    add auth_system, basket, menu, story; add static including css, img & js (just in case)

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'auth_system',
+    'basket',
+    'menu',
+    'story',
+
 ]
 
 MIDDLEWARE = [
