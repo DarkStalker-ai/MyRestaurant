95547d9bcfb2cd3bf0b00f06b7fcb10b169af03f
dark_st <tatarinov.tima6@gmail.com>
2025-06-19 13:52:34 +0300

    add models, urls & views in auth_system

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('login/', 'auth_system.views.login_view', name='login'),
+    path('logout/', 'auth_system.views.logout_view', name='logout'),
+    path('register/', 'auth_system.views.register_view', name='register'),
+    path('profile/', 'auth_system.views.profile_view', name='profile'),
+    path('profile/edit/', 'auth_system.views.edit_profile_view', name='edit_profile'),
 ]
--- auth_system/views.py
+++ auth_system/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from .forms import RegistrationForm, EditProfileForm
 
 # Create your views here.
 def login_view(request):
@@ -10,8 +13,45 @@ def login_view(request):
         if user:
             login(request, user)
 
-            return redirect('novations')
+            return redirect('')
         else:
             messages.error(request, "Неправильне ім'я користувача або пароль")
 
     return render(request, 'auth_system/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('')
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('')
+    else:
+        form = RegistrationForm()
+    return render(request, 'auth_system/register.html', {'form': form})
+
+def profile_view(request):
+    avatar = 'profile/default_avatar.png'
+    context = {
+        'photo': avatar,
+        'user': request.user,
+    }
+
+    return render(request, 'auth_system/profile.html', context)
+
+def edit_profile_view(request):
+    profile = get_object_or_404(Profile, user=request.user)
+    if request.method == "POST":
+        avatar = request.FILES.get("image")
+        bio = request.POST.get("bio")
+        profile.bio = bio
+        if avatar:
+            profile.avatar = avatar
+        profile.save()
+        return redirect('portfolio')
+    else:
+        return render(request, 'auth_system/edit_profile.html', {'profile': profile} )
