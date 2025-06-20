9bbd4515417d0ce993e54e5f68441d2599affbc4
dark_st <tatarinov.tima6@gmail.com>
2025-06-20 19:18:11 +0300

    add menu.html in menu; add menu in views.py; add new path in urls.py; edited dish_list & _details; add auth_system in settings.py

--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    path('', views.dish_list, name='dish_list'),
-    path('dish/<int:pk>/', views.dish_detail, name='dish_detail'),
+    path('', views.menu, name='menu'),
+    path('menu/', views.dish_list, name='dish_list'),
+    path('menu/category/<int:pk>/', views.dish_list, name='category_detail'),
+    path('menu/dish/<int:pk>/', views.dish_detail, name='dish_detail'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Dish
+from .models import Dish, Category
 
 # Create your views here.
 
+def menu(request):
+    categories = Category.objects.all()
+    return render(request, '../templates/menu.html', {'categories': categories})
+
 def dish_list(request):
-    dishes = Dish.objects.all()
-    return render(request, '../templates/dish_list.html', {'dishes': dishes})
+    if category_id:
+        category = get_object_or_404(Category, pk=category_id)
+        dishes = Dish.objects.filter(category=category)
+    else:
+        dishes = Dish.objects.all()
+        category = None
+
+    categories = Category.objects.all()
+    return render(request, '../templates/menu/dish_list.html', {
+        'dishes': dishes,
+        'categories': categories,
+        'current_category': category
+    })
 
 def dish_detail(request, pk):
     dish = get_object_or_404(Dish, pk=pk)
-    return render(request, '../templates/dish_detail.html', {'dish': dish})
+    return render(request, '../templates/menu/dish_details.html', {'dish': dish})
